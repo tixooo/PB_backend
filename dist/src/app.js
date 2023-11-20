04ec3c7b9f986e2154aa376aef1f159f893398d2
Konstantin <konstantin.stefanov87@gmail.com>
2023-11-21 00:27:59 +0200

    did a lot of shit

--- dist/src/app.js
+++ dist/src/app.js
@@ -15,12 +15,45 @@ Object.defineProperty(exports, "__esModule", { value: true });
 const express_1 = __importDefault(require("express"));
 const db_1 = __importDefault(require("./db"));
 const paste_model_1 = __importDefault(require("../models/paste_model"));
+const body_parser_1 = __importDefault(require("body-parser"));
+const xss_1 = __importDefault(require("xss"));
 const app = (0, express_1.default)();
 (0, db_1.default)();
+app.use(body_parser_1.default.json());
+app.use((req, res, next) => {
+    res.setHeader('Content-Type', 'application/json');
+    res.setHeader('X-Powered-By', 'Node.js');
+    next();
+});
+app.post('/pastes', (req, res) => __awaiter(void 0, void 0, void 0, function* () {
+    const pasteContent = (0, xss_1.default)(req.body.content);
+    const paste = new paste_model_1.default({
+        content: pasteContent,
+    });
+    try {
+        yield paste.save();
+        res.status(201).json(paste);
+    }
+    catch (error) {
+        res.status(400).json({ message: error.message });
+    }
+}));
+app.delete('/pastes/:id', (req, res) => __awaiter(void 0, void 0, void 0, function* () {
+    try {
+        const paste = yield paste_model_1.default.findById(req.params.id);
+        if (!paste) {
+            return res.status(404).json({ message: 'Paste not found' });
+        }
+        yield paste.deleteOne();
+        res.json({ message: 'Paste deleted' });
+    }
+    catch (error) {
+        res.status(500).json({ message: error.message });
+    }
+}));
 app.get('/', (req, res) => __awaiter(void 0, void 0, void 0, function* () {
     const pastes = yield paste_model_1.default.find();
     res.json(pastes);
-    console.log(res.json(pastes));
 }));
 app.listen(3000, () => {
     console.log('Server is running at http://localhost:3000');
--- src/app.ts
+++ src/app.ts
@@ -1,15 +1,67 @@
 import express from 'express';
 import ConnectD from "./db";
 import Paste from "../models/paste_model";
+import bodyParser  from "body-parser";
+import xss from "xss";
 
 const app = express();
 ConnectD()
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+
+app.use((req, res, next) => {
+  res.setHeader('Content-Type', 'application/json');
+  res.setHeader('X-Powered-By', 'Node.js');
+  next();
+})
+
+app.get('/pastes', async (req, res) => {
+  const pastes = await Paste.find()
+  res.json(pastes)
+});
+
+app.get('/pastes/:id', async (req, res) => {
+  try {
+    const paste = await Paste.findById(req.params.id);
+    if (!paste) {
+      return res.status(404).json({ message: 'Paste not found' });
+    }
+    res.json(paste);
+  } catch (error) {
+    res.status(500).json({ message: error.message });
+  }
+});
 app.get('/', async (req, res) => {
   const pastes = await Paste.find()
   res.json(pastes)
-  console.log(res.json(pastes))
 });
 
+
+app.post('/pastes', async (req, res) => {
+  const pasteContent = xss(req.body.content);
+  const paste = new Paste({
+    content: pasteContent,
+  });
+  try {
+    await paste.save();
+    res.status(201).json(paste);
+  } catch (error) {
+    res.status(400).json({ message: error.message });
+  }
+});
+
+app.delete('/pastes/:id', async (req, res) => {
+  try {
+    const paste = await Paste.findById(req.params.id);
+    if (!paste) {
+      return res.status(404).json({ message: 'Paste not found' });
+    }
+    await paste.deleteOne();
+    res.json({ message: 'Paste deleted' });
+  } catch (error) {
+    res.status(500).json({ message: error.message });
+  }
+});
 app.listen(3000, () => {
   console.log('Server is running at http://localhost:3000');
 });
